460bfffa818c7e0c433b610198efe1a5c2aa7850
Ivan Movchan <ivan.movchan.07@gmail.com>
2025-05-28 19:00:15 +0300

    New code examples!
    1. Uploaded new programs:
       * hello-world.s - an updated "Hello World" program using string literals;
       * escape-sequences.s - a program demonstrating escape sequences processing by HyperCPU;
       * compare.s - a program demonstrating compare and conditional jump commands.
    2. Renamed hello-world.s to hello-world-legacy.s.
    
    Signed-off-by: Ivan Movchan <ivan.movchan.07@gmail.com>

---
+++ examples/hello-world-legacy.s
@@ -0,0 +1,25 @@
+/*
+ * This is a simple "Hello World" program written in HyperCPU assembly (hASM).
+ * It writes a string to the console char by char, then writes a newline byte (0x10) and halts CPU.
+ *
+ * If you are interested in more modern (and simple?) way of displaying text in console, see hello-world.s.
+ *
+ * Written by Ivan Movchan <ivan.movchan.07@gmail.com> in 2025.
+ */
+
+main:
+	write xlll0, 'H';
+	write xlll0, 'e';
+	write xlll0, 'l';
+	write xlll0, 'l';
+	write xlll0, 'o';
+	write xlll0, ',';
+	write xlll0, ' ';
+	write xlll0, 'w';
+	write xlll0, 'o';
+	write xlll0, 'r';
+	write xlll0, 'l';
+	write xlll0, 'd';
+	write xlll0, '!';
+	write xlll0, 0u10;
+	halt;
---
+++ examples/hello-world.s
@@ -0,0 +1,26 @@
+/*
+ * This is another simple "Hello World" program written in HyperCPU assembly (hASM).
+ * It writes a null-terminated string to the console using string literals and a little loop, then halts CPU.
+ *
+ * Before 0.4.5 version you could have to write a string char by char — see hello-world-legacy.s for details.
+ *
+ * Written by Ivan Movchan <ivan.movchan.07@gmail.com> in 2025.
+ */
+
+data:
+	.b8 "Hello, world!\n";
+	.b8 0u0;
+
+.attr(entry) main:      // Program entry point.
+	mov x1, data;
+
+loop:
+	mov xlll2, [x1];
+	cmp xlll2, 0u0;     // Is NULL (0u0)?
+	jme end;            // Yes — go to finish.
+	write xlll0, xlll2; // No — write data to console.
+	inc x1;
+	jmp loop;
+
+end:
+	halt;
