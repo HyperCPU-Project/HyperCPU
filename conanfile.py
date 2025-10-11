abf4438d0933155180653d57afd94ac11ebaee55
HyperWinX <artmsmth@gmail.com>
2025-10-11 12:40:55 +0400

    Start writing new config

---
+++ conanfile.py
@@ -0,0 +1,20 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+
+class HyperCPU(ConanFile):
+    name = "HyperCPU"
+    version = "0.5.0"
+    settings = ["os", "compiler", "build_type", "arch"]
+    author = "HyperCPU Project"
+    requires = [
+        "gtest/1.14.0",
+        "spdlog/1.15.0",
+        "argparse/3.2",
+        "eternal/1.0.1",
+        "fmt/11.0.2"
+    ]
+    generators = ["CMakeToolchain", "CMakeDeps"]
+
+    def layout(self) -> None:
+        cmake_layout(self)
